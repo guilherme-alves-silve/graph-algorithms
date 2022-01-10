b462cc91162e47bb63201a6e45873da060b8cb2b
Guilherme Alves Silveira <guilherme.alves.silve@gmail.com>
2022-01-10 01:42:51 -0300

    Implement the undirect path algorithm with depth first search and breadth first search

---
+++ src/main/java/br/com/guilhermealvessilve/graph/algorithms/undirectedpath/UndirectedPathBreadthFirstSearch.java
@@ -0,0 +1,66 @@
+package br.com.guilhermealvessilve.graph.algorithms.undirectedpath;
+
+import java.util.*;
+
+import static java.util.Map.Entry;
+import static java.util.Map.entry;
+
+public class UndirectedPathBreadthFirstSearch {
+
+    public static void main(String[] args) {
+        var edges = List.of(
+                entry("i", "j"),
+                entry("k", "i"),
+                entry("m", "k"),
+                entry("k", "l"),
+                entry("o", "n")
+        );
+
+        System.out.println("undirectedPath(edges, 'j', 'm'): " + undirectedPath(edges, "j", "m")); // true
+        System.out.println("undirectedPath(edges, 'm', 'j'): " + undirectedPath(edges, "m", "j")); // -> true
+        System.out.println("undirectedPath(edges, 'l', 'j'): " + undirectedPath(edges, "l", "j")); // -> true
+        System.out.println("undirectedPath(edges, 'k', 'o'): " + undirectedPath(edges, "k", "o")); // -> false
+        System.out.println("undirectedPath(edges, 'i', 'o'): " + undirectedPath(edges, "i", "o")); // -> false
+    }
+
+    public static boolean undirectedPath(List<Entry<String, String>> edges, String nodeA, String nodeB) {
+        var graph = buildGraph(edges);
+        return hasPath(graph, nodeA, nodeB, new HashSet<>());
+    }
+
+    private static Map<String, List<String>> buildGraph(List<Entry<String, String>> edges) {
+        var graph = new HashMap<String, List<String>>();
+        for (var edge : edges) {
+            var nodeA = edge.getKey();
+            var nodeB = edge.getValue();
+            if (!graph.containsKey(nodeA)) graph.put(nodeA, new ArrayList<>());
+            if (!graph.containsKey(nodeB)) graph.put(nodeB, new ArrayList<>());
+
+            graph.get(nodeA).add(nodeB);
+            graph.get(nodeB).add(nodeA);
+        }
+
+        return graph;
+    }
+
+    private static boolean hasPath(Map<String, List<String>> graph, String src, String dest, Set<String> visited) {
+
+        var queue = new LinkedList<String>();
+        queue.push(src);
+
+        while (!queue.isEmpty()) {
+            var current = queue.pop();
+            visited.add(current);
+
+            if (current.equals(dest)) return true;
+
+            for (var neighbor : graph.get(current)) {
+                if (!visited.contains(neighbor)) {
+                    queue.push(neighbor);
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/br/com/guilhermealvessilve/graph/algorithms/undirectedpath/UndirectedPathDepthFirstSearch.java
@@ -0,0 +1,61 @@
+package br.com.guilhermealvessilve.graph.algorithms.undirectedpath;
+
+import java.util.*;
+
+import static java.util.Map.*;
+
+public class UndirectedPathDepthFirstSearch {
+
+    public static void main(String[] args) {
+        var edges = List.of(
+                entry("i", "j"),
+                entry("k", "i"),
+                entry("m", "k"),
+                entry("k", "l"),
+                entry("o", "n")
+        );
+
+        System.out.println("undirectedPath(edges, 'j', 'm'): " + undirectedPath(edges, "j", "m")); // true
+        System.out.println("undirectedPath(edges, 'm', 'j'): " + undirectedPath(edges, "m", "j")); // -> true
+        System.out.println("undirectedPath(edges, 'l', 'j'): " + undirectedPath(edges, "l", "j")); // -> true
+        System.out.println("undirectedPath(edges, 'k', 'o'): " + undirectedPath(edges, "k", "o")); // -> false
+        System.out.println("undirectedPath(edges, 'i', 'o'): " + undirectedPath(edges, "i", "o")); // -> false
+    }
+
+    public static boolean undirectedPath(List<Entry<String, String>> edges, String nodeA, String nodeB) {
+        var graph = buildGraph(edges);
+        return hasPath(graph, nodeA, nodeB, new HashSet<>());
+    }
+
+    private static Map<String, List<String>> buildGraph(List<Entry<String, String>> edges) {
+        var graph = new HashMap<String, List<String>>();
+        for (var edge : edges) {
+            var nodeA = edge.getKey();
+            var nodeB = edge.getValue();
+            if (!graph.containsKey(nodeA)) graph.put(nodeA, new ArrayList<>());
+            if (!graph.containsKey(nodeB)) graph.put(nodeB, new ArrayList<>());
+
+            graph.get(nodeA).add(nodeB);
+            graph.get(nodeB).add(nodeA);
+        }
+
+        return graph;
+    }
+
+    private static boolean hasPath(Map<String, List<String>> graph,
+                                   String src,
+                                   String dest,
+                                   Set<String> visited) {
+        if (src.equals(dest)) return true;
+        if (visited.contains(src)) return false;
+        visited.add(src);
+
+        for (var neighbor : graph.get(src)) {
+            if (hasPath(graph, neighbor, dest, visited)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
